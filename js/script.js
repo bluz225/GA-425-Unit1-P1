d5484bc88f985397cc6265ee16bf9bee149e1b18
Billy Lu <billyfwlu@gmail.com>
2022-05-06 21:11:32 -0700

    rendered projectile shape for testing

--- js/script.js
+++ js/script.js
@@ -17,14 +17,16 @@ class GenerateCannon{
         this.originX = originX
         this.originY = originY
         this.deg = 45 // degrees
+        this.dir = 1
+        this.truDeg = 0
+        this.cannonMx = 0
+        this.cannonMy = 0
     }
 
     renderCannon(mouseX,mouseY){
-
-    let cannonOx = 0 
-    let cannonOy = 0
-    let cannonMx = 0
-    let cannonMy = 0
+        if (this.originX > centerline) {
+            this.dir = -1
+        }
     let cannonLength = 45*scale
     let rad = 0
     let cannon = new Path2D()
@@ -38,61 +40,86 @@ class GenerateCannon{
 
     if (this.deg < 90) {
         rad = convertDegtoRad(this.deg)
-        cannonMx = cannonLength*Math.cos(rad)
-        cannonMy =  cannonLength*Math.sin(rad)  
+        this.cannonMx = this.originX+cannonLength*Math.cos(rad)*this.dir
+        this.cannonMy = this.originY-cannonLength*Math.sin(rad)  
         cannon.moveTo(this.originX, this.originY)
-        cannon.lineTo(this.originX+cannonMx, this.originY-cannonMy)
+        cannon.lineTo(this.cannonMx, this.cannonMy)
+        this.truDeg = this.deg
+        // this.truDeg = this.deg
+        // console.log("<90 degree", this.truDeg)
 
     } else if (this.deg > 90){
         rad = convertDegtoRad(this.deg-90)
-        cannonMx = cannonLength*Math.sin(rad)
-        cannonMy =  cannonLength*Math.cos(rad)
+        this.cannonMx = this.originX-cannonLength*Math.sin(rad)*this.dir
+        this.cannonMy = this.originY-cannonLength*Math.cos(rad)
         cannon.moveTo(this.originX, this.originY)
-        cannon.lineTo(this.originX-cannonMx, this.originY-cannonMy)
+        cannon.lineTo(this.cannonMx, this.cannonMy)
+        this.truDeg = this.deg
+
+        // this.truDeg = this.truDeg + convertRadtoDeg(rad)
+        // console.log(">90 degree", this.truDeg)
 
     } else if (this.deg === 90) {
+        this.cannonMx = this.originX
+        this.cannonMy = this.originY-cannonLength
         cannon.moveTo(this.originX, this.originY)
-        cannon.lineTo(this.originX,this.originY-cannonLength)
+        cannon.lineTo(this.cannonMx, this.cannonMy)
+        this.truDeg = 90
 
     } else if (this.deg === 0) {
+        this.cannonMx = this.originX+cannonLength
+        this.cannonMy = this.originY
         cannon.moveTo(this.originX, this.originY)
-        cannon.lineTo(this.originX+cannonLength, this.originY)
+        cannon.lineTo(this.cannonMx, this.cannonMy)
+        this.truDeg = 0
 
     } else if (this.deg === 180) {
+        this.cannonMx = this.originX-cannonLength
+        this.cannonMy = this.originY
         cannon.moveTo(this.originX, this.originY)
-        cannon.lineTo(this.originX-cannonLength, this.originY)
+        cannon.lineTo(this.cannonMx, this.cannonMy)
+        this.truDeg = 180
 
     }
-    
-    // console.log("x:", cannonMx,"y:", cannonMy,"deg:", this.deg, "rad:", rad)
-    // console.log("hypon:", Math.sqrt(Math.pow(cannonMx,2)+Math.pow(cannonMy,2)))
+    // console.log("tru deg:",this.truDeg)
+    // console.log("x:", cannonMx,"y:", this.cannonMy,"deg:", this.deg, "rad:", rad)
+    // console.log("hypon:", Math.sqrt(Math.pow(cannonMx,2)+Math.pow(this.cannonMy,2)))
     cannon.closePath()
     // gameplayCtx.strokeStyle = this.color
     gameplayCtx.stroke(cannon)
-
-
     }
 
 }
 
 class projectile {
     constructor(Xi,Yi,Vi,Deg){
+        this.side = 10
         this.t = 0
         this.Vi = Vi
         this.Deg = Deg
         this.Xi = Xi
         this.Yi = Yi
+        this.Xf = 1
+        this.Yf = 1
+        this.centerXi = this.Xi - this.side/2
+        this.centerYi = this.Yi - this.side/2
+        // this.centerXf = 0
+        // this.centerYf = 0
 
     }
 
     render(){
-
+        console.log(this.Deg)
+        
+        gameplayCtx.fillStyle = "red"
+        gameplayCtx.fillRect(this.centerXi,this.centerYi,this.side,this.side)
     }
 
 }
 
 class Turret {
-    constructor(x,y,side,color){
+    constructor(x,y,side,color,shape){
+        this.shape = shape
         this.x = x
         this.y = y
         this.side = side
@@ -137,6 +164,7 @@ const grav = 9.81
 let menufront = true
 let gameplayCanvasHeight = gameplayCanvas.height
 let gameplayCanvasWidth = gameplayCanvas.width
+let centerline = gameplayCanvas.width/2
 const side = 50*scale
 let PlayerTriXPos = 100*scale
 let PlayerTriColor = "black"
@@ -153,10 +181,11 @@ let currCannon
 let pauseState = false
 
 // Create 
-const triangle = new TriangleTurrent(PlayerTriXPos/scale,gameFloor,side,PlayerTriColor)
-const square = new Turret(PlayerSqXPos/scale,gameFloor,side,PlayerSqColor)
+const triangle = new TriangleTurrent(PlayerTriXPos/scale,gameFloor,side,PlayerTriColor,"triangle")
+const square = new Turret(PlayerSqXPos/scale,gameFloor,side,PlayerSqColor,"square")
 const SqCannon = new GenerateCannon(square.centerx,square.centery)
 const TriCannon = new GenerateCannon(triangle.centerx,triangle.centery)
+
 
 //DOM CONTENT LOADED HERE
 document.addEventListener("DOMContentLoaded", function(){
@@ -241,13 +270,6 @@ document.addEventListener("DOMContentLoaded", function(){
     
 })
 
-
-
-    
-
-
-
-
 //FUNCTIONS BELOW HERE
 function pause(arg){
     if (arg === true){
@@ -267,6 +289,8 @@ function gameLoop(){
     triangle.renderTurret()
     TriCannon.renderCannon(MouseCurrX,MouseCurrY)
     SqCannon.renderCannon(MouseCurrX,MouseCurrY)
+    const miss = new projectile(TriCannon.cannonMx,TriCannon.cannonMy,30,currCannon.truDeg)
+    miss.render()
 }
 
 
